7c7c3e9771390e9a0d33e71af847b3639316ae28
uinpay <uinpay@uinpaydeMac-mini.local>
2019-07-19 09:39:57 +0800

    add project

---
+++ toolsLib/toolsLib/toolsLib.h
@@ -0,0 +1,13 @@
+//
+//  toolsLib.h
+//  toolsLib
+//
+//  Created by uinpay on 2019/7/19.
+//  Copyright © 2019 刘起伏. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface toolsLib : NSObject
+
+@end
---
+++ toolsLib/toolsLib/toolsLib.m
@@ -0,0 +1,13 @@
+//
+//  toolsLib.m
+//  toolsLib
+//
+//  Created by uinpay on 2019/7/19.
+//  Copyright © 2019 刘起伏. All rights reserved.
+//
+
+#import "toolsLib.h"
+
+@implementation toolsLib
+
+@end
